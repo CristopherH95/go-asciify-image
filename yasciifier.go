fb0debe8a4948f5e669317c649d831ec63891726
Cristopher Hernandez <DoughboysWWI@gmail.com>
2019-03-15 15:38:11 -0700

    Update .gitignore, refactor code, modify resizing logic

--- yasciifier.go
+++ yasciifier.go
@@ -12,14 +12,14 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		printUsage()	// need to be provided an argument for the file path
+		printUsage() // need to be provided an argument for the file path
 		return
 	}
-	path := os.Args[1]	// grab the path
-	if _, err := os.Stat(path); err == nil {	// if the file exists begin processing
+	path := os.Args[1]                       // grab the path
+	if _, err := os.Stat(path); err == nil { // if the file exists begin processing
 		fmt.Printf("Processing file: %s\n", path)
-		file := ConvertImageToAscii(path)	// convert image to ascii
-		fmt.Printf("Output saved to file: %s\n", file)	// tell user where the output can be found
+		file := ConvertImageToAscii(path)              // convert image to ascii
+		fmt.Printf("Output saved to file: %s\n", file) // tell user where the output can be found
 	} else {
 		log.Print(err)
 		fmt.Println("Could not find image file to read in.")
@@ -29,11 +29,11 @@ func main() {
 // Converts an image at the given file path to ascii and then writes the results to a file
 // The new file will have the same file name as the image, but with .txt appended to it
 func ConvertImageToAscii(path string) string {
-	pix, err := getImagePixels(path)	// get image pixels
+	pix, err := getImagePixels(path) // get image pixels
 	checkErr(err)
-	pixVal := pixelMatrixToBrightness(pix)	// get brightness values from pixels
-	ascii := brightnessMatrixToAscii(pixVal)	// convert brightness to corresponding ascii
-	file := writeMatrixToFile(path, ascii)	// write results to a file
+	pixVal := convertRGBToBrightness(pix)     // get brightness values from pixels
+	ascii := convertBrightnessToAscii(pixVal) // convert brightness to corresponding ascii
+	file := writeMatrixToFile(path, ascii)    // write results to a file
 	return file
 }
 
@@ -57,11 +57,11 @@ func printUsage() {
 // Writes the given ascii matrix to a new file with the same name as the given file path
 // but with .txt appended to it
 func writeMatrixToFile(path string, ascii [][]byte) string {
-	file := path + ".txt"	// add .txt to original file name for new file
-	f, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY, 0644)	// create file (or overwrite)
+	file := path + ".txt"                                      // add .txt to original file name for new file
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY, 0644) // create file (or overwrite)
 	checkErr(err)
 	for _, row := range ascii {
-		_, err := f.Write(row)	// write data, if error encountered try to cleanup the file
+		_, err := f.Write(row) // write data, if error encountered try to cleanup the file
 		if err != nil {
 			delErr := os.Remove(file)
 			checkErr(delErr)
@@ -73,7 +73,7 @@ func writeMatrixToFile(path string, ascii [][]byte) string {
 }
 
 // Returns an image at the given path along with its width and height
-// The image is re-sized if it's width and height are over 200 pixels each
+// The image is re-sized if it's width or height is over 200 pixels each
 // This is to prevent the ascii version from being too large
 func getImageData(path string) (image.Image, int, int) {
 	file, err := os.Open(path)
@@ -83,11 +83,12 @@ func getImageData(path string) (image.Image, int, int) {
 	bounds := img.Bounds()
 	width := bounds.Max.X
 	height := bounds.Max.Y
-	if width > 200 && height > 200 {
+	if width > 200 || height > 200 {
+		// image may be big, resize and preserve aspect ratio
 		if width > height {
-			img = imaging.Resize(img, 200, 0, imaging.Lanczos)
+			img = imaging.Resize(img, 200, 0, imaging.Lanczos)	// resize with width being larger
 		} else {
-			img = imaging.Resize(img, 0, 200, imaging.Lanczos)
+			img = imaging.Resize(img, 0, 200, imaging.Lanczos)	// resize with height being larger
 		}
 		bounds = img.Bounds()
 		width = bounds.Max.X
@@ -105,7 +106,7 @@ func getImagePixels(path string) ([][]Pixel, error) {
 	for y := 0; y < height; y++ {
 		var row []Pixel
 		for x := 0; x < width; x++ {
-			row = append(row, rgbaToRGB(img.At(x, y).RGBA()))
+			row = append(row, convertRGBAToRGB(img.At(x, y).RGBA()))
 		}
 		pixels = append(pixels, row)
 	}
@@ -114,12 +115,12 @@ func getImagePixels(path string) ([][]Pixel, error) {
 }
 
 // Converts all the given Pixels from their RGB format to a single brightness value
-func pixelMatrixToBrightness(pixels [][]Pixel) [][]uint32 {
+func convertRGBToBrightness(pixels [][]Pixel) [][]uint32 {
 	var brightness [][]uint32
 	for y := 0; y < len(pixels); y++ {
 		var row []uint32
 		for x := 0; x < len(pixels[y]); x++ {
-			row = append(row, uint32((pixels[y][x].R + pixels[y][x].G + pixels[y][x].B) / 3))
+			row = append(row, uint32((pixels[y][x].R+pixels[y][x].G+pixels[y][x].B)/3))
 		}
 		brightness = append(brightness, row)
 	}
@@ -128,7 +129,7 @@ func pixelMatrixToBrightness(pixels [][]Pixel) [][]uint32 {
 }
 
 // Converts a matrix of brightness values into ascii values which approximate that brightness
-func brightnessMatrixToAscii(brightness [][]uint32) [][]byte {
+func convertBrightnessToAscii(brightness [][]uint32) [][]byte {
 	chars := "\"`^\\\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 	log.Println("Converting brightness matrix to ascii")
 	var ascii [][]byte
@@ -136,7 +137,7 @@ func brightnessMatrixToAscii(brightness [][]uint32) [][]byte {
 		var row []byte
 		for _, val := range values {
 			charVal := chars[getStringRelativeIndex(val, 255, chars)]
-			row = append(row, charVal, charVal)	// add item more than once to prevent final result from looking stretched
+			row = append(row, charVal, charVal) // add item more than once to prevent final result from looking stretched
 		}
 		row = append(row, '\n')
 		ascii = append(ascii, row)
@@ -149,7 +150,7 @@ func brightnessMatrixToAscii(brightness [][]uint32) [][]byte {
 func getStringRelativeIndex(val uint32, maxVal uint32, chars string) int {
 	valPercent := float32(val) / float32(maxVal)
 	idx := int(float32(len(chars)) * float32(valPercent))
-	return clampInt(idx, 0, len(chars) - 1)
+	return clampInt(idx, 0, len(chars)-1)
 }
 
 // Clamps the given integer between min and max values
@@ -163,7 +164,7 @@ func clampInt(val int, minVal int, maxVal int) int {
 }
 
 // Converts RGBA to an RGB based Pixel
-func rgbaToRGB(rgba ...uint32) Pixel {
+func convertRGBAToRGB(rgba ...uint32) Pixel {
 	return Pixel{int(rgba[0] / 257), int(rgba[1] / 257), int(rgba[2] / 257)}
 }
 
